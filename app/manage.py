264e18fbb3667a14c8e49a93ff432e0571233a31
kyakulumbye ahmad <kyakusahmed@outlook.com>
2018-11-08 13:27:36 +0300

    [Delivers #161781118] add api endpoint that helps to cancel a specific parcel order

--- app/manage.py
+++ app/manage.py
@@ -4,13 +4,25 @@ from flask import request
 delivery_orders = []
 
 class Order:
-    """Class to manipulate delivery order."""
 
     def __init__(self):
         self.delivery_orders = delivery_orders
 
-    def get_all_orders(self):
-        """Get list of all orders."""
-        return self.delivery_orders
+    def search_order(self, id):
+        order = [order for order in self.delivery_orders if order['id'] == int(id)]
+        if order:
+            return order
+        return None 
 
-    
+    def cancel_parcel_delivery_order(self, id, status):
+        order = self.search_order(id)
+        if order:
+            order[0].update({"status": status})
+            return order
+        return "Order not found"        
+
+
+
+
+   
+   
--- app/views.py
+++ app/views.py
@@ -4,8 +4,9 @@ from .manage import Order
 app2 = Flask(__name__)
 order = Order()
 
-@app2.route('/api/v1/parcel_orders', methods=['GET'])
-def get_all_orders():
-    """Get all orders"""
-    return jsonify({'orders': order.get_all_orders()}), 200
-
+@app2.route('/api/v1/orders/<int:id>', methods=['PUT'])
+def cancel_parcel_delivery_order(id):
+    get_input = request.get_json()
+    if not get_input.get("status"):
+        return jsonify({"error" : "status is required"}), 200
+    return jsonify({"order" : order.cancel_parcel_delivery_order(id, get_input["status"])}), 200
