f10bd055ae5e76b8b1c1f5ab6d05c9f3d106aea9
kyakulumbye ahmad <kyakusahmed@outlook.com>
2018-11-08 13:03:02 +0300

    [Delivers #161781013] add api endpoint that helps me get a specific parcel order

--- app/views.py
+++ app/views.py
@@ -4,18 +4,13 @@ from .manage import Order
 app2 = Flask(__name__)
 order = Order()
 
-@app2.route('/api/v1/delivery_orders', methods=['POST'])
-def add_one():
-    data = request.get_json()
-    if not data.get("recipient"):
-        return jsonify({"error": "recipient is required"}), 200
-    elif not data.get("sender"):
-        return jsonify({"error": "sender is required"}), 200
-    elif not data.get('parcel_details'):
-        return jsonify({"error": "parcel_details required"}), 200
-    return jsonify({"order": order.add_order(
-                data["recipient"],
-                data["sender"],
-                data["parcel_details"]
-            )}), 201
+
+
+@app2.route('/api/v1/delivery_orders/<int:id>', methods=['GET'])
+def get_order(id):
+    """get specific order."""
+    parcel_order = order.search_order(id)
+    if not parcel_order:
+        return jsonify({"message": 'order not found'}), 404
+    return jsonify({"order": parcel_order}), 200     
 
