8f3f3e0dd4a1a2ee2d376896eb11fdb64f5feac8
kyakulumbye ahmad <kyakusahmed@outlook.com>
2018-11-09 17:19:05 +0300

    [Delivers #161780881] update add_order api endpoint

--- app/views.py
+++ app/views.py
@@ -4,9 +4,7 @@ from app.manage import Parcel
 app2 = Flask(__name__)
 parcel = Parcel()
 
-@app2.route('/api/v1/parcels', methods=['GET'])
-def get_all_orders():
-    return jsonify({'parcels': parcel.get_all_parcels()}), 200
+
 
 @app2.route('/api/v1/parcels', methods=['POST'])
 def add_parcel():
@@ -40,33 +38,3 @@ def add_parcel():
     )
     return jsonify({ "parcel": save_parcel}), 201
     
-@app2.route('/api/v1/parcel/<int:id>', methods=['GET'])
-def get_parcel(id):
-    """get specific order."""
-    parcel2 = parcel.search_parcel(id)
-    if not parcel2:
-        return jsonify({"message": 'parcel not found'}), 404
-    return jsonify({"parcel": parcel2}), 200  
-#  return jsonify({"order": parcel_[0]['status']}), 200   
-
-@app2.route('/api/v1/parcels/<int:id>', methods=['PUT'])
-def update_status(id):
-    get_input = request.get_json()
-    if not get_input.get("status"):
-        return jsonify({"error" : "status is required"}), 200
-    if not get_input.get("sender"):
-        return jsonify({"error" : "sender is required"}), 200
-    parcel2 = parcel.search_parcel(id)
-    if not parcel2:
-        return jsonify({"message": 'parcel not found'}), 404
-    else:
-        if parcel2[0]['sender_id'] != get_input["sender"]:
-            return jsonify({"message": 'parcel not yours'}), 400
-        if parcel2[0]['status'] == "Delivered":
-            return jsonify({"message": 'parcel already delivered'}), 400
-        return jsonify({"parcel" : parcel.update_parcel_status(id, get_input["status"])}), 200
-
-@app2.route('/api/v1/parcels/sender/<int:sender_id>', methods=['GET'])
-def get_all_parcels_by_user(sender_id):
-    sender_parcels = parcel.search_sender_parcels(sender_id)
-    return jsonify({ 'parcels': sender_parcels })
