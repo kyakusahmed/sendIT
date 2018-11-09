724ff8329a3ead1bc0e2cd7f5cd9b8ac034c90a7
kyakulumbye ahmad <kyakusahmed@outlook.com>
2018-11-09 17:26:42 +0300

    [Delivers #161781013] update a Get specific parcel delivery order api endpoint

--- app/views.py
+++ app/views.py
@@ -3,36 +3,11 @@ from app.manage import Parcel
 
 app2 = Flask(__name__)
 parcel = Parcel()
-
-@app2.route('/api/v1/parcels', methods=['POST'])
-def add_parcel():
-    data = request.get_json()
-    if not data.get("recepient_name"):
-        return jsonify({"error": "recipient name is required"}), 200
-    elif not data.get("recepient_phone"):
-        return jsonify({"error": "recipient phone is required"}), 200
-    elif not data.get("recepient_country"):
-        return jsonify({"error": "recipient country is required"}), 200
-    elif not data.get("recepient_destination"):
-        return jsonify({"error": "recipient destination is required"}), 200
-    elif not data.get("sender_id"):
-        return jsonify({"error": "sender is required"}), 200
-    elif not data.get("location"):
-        return jsonify({"error": "sender location is required"}), 200
-    elif not data.get('weight'):
-        return jsonify({"error": "weight required"}), 200
-    elif not data.get('price'):
-        return jsonify({"error": "price required"}), 200
-
-    save_parcel = parcel.add_parcel(
-        data['sender_id'], 
-        data['location'], 
-        data['recepient_name'],
-        data['recepient_phone'],
-        data['recepient_country'],
-        data['recepient_destination'],
-        data['weight'],
-        data['price'],
-    )
-    return jsonify({ "parcel": save_parcel}), 201
     
+@app2.route('/api/v1/parcel/<int:id>', methods=['GET'])
+def get_parcel(id):
+    """get specific order."""
+    parcel2 = parcel.search_parcel(id)
+    if not parcel2:
+        return jsonify({"message": 'parcel not found'}), 404
+    return jsonify({"parcel": parcel2}), 200  
