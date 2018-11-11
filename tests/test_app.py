f633b792278fe03d7e2f6ce3b56f3c54a839fc8d
kyakulumbye ahmad <kyakusahmed@outlook.com>
2018-11-11 12:42:59 +0300

    [Delivers #161808422] add tests folder

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,7 @@ class OrderTest(unittest.TestCase):
     def setUp(self):
         self.app = app2.test_client()
         self.app.testing = True
-        self.parcel = { 
+        self.test_parcel = { 
             "country": "uganda",
             "destination": "kampala",
             "parcel_price": "500k",
@@ -28,21 +28,21 @@ class OrderTest(unittest.TestCase):
         self.assertIsInstance(data['parcels'], list)
 
     def test_get_parcel(self):
-        self.app.post('/api/v1/parcels', json=self.parcel)
+        self.app.post('/api/v1/parcels', json=self.test_parcel)
         response = self.app.get('/api/v1/parcels/1')
         assert response.status_code == 404
 
     def test_update_status(self):
-        self.app.post('/api/v1/parcels', json=self.parcel)
+        self.app.post('/api/v1/parcels', json=self.test_parcel)
         response =self.app.put('/api/v1/parcels/1', json={"status": "pending", "sender": 1})
-        data = json.loads(response.get_data(as_text=True))
+        # data = json.loads(response.get_data(as_text=True))
         assert response.status_code == 404
         # self.assertIsInstance(data, list)
         
         # self.assertEqual(json.loads(response.data.decode('utf-8'))['data']['status'], "cancelled")
 
     def test_update_status_not_found(self):
-        self.app.post('/api/v1/parcels', json=self.parcel)
+        self.app.post('/api/v1/parcels', json=self.test_parcel)
         response =self.app.get('/api/v1/parcels/12345')
         data = json.loads(response.get_data(as_text=True))
         assert data["message"] == "parcel not found"
@@ -78,32 +78,69 @@ class OrderTest(unittest.TestCase):
         self.assertIsInstance(data['parcels'], list)
    
     def test_add_parcel(self):
-        response=self.app.post('/api/v1/parcels', json=self.parcel)
+        response=self.app.post('/api/v1/parcels', json=self.test_parcel)
         data = json.loads(response.get_data(as_text=True))
         assert response.status_code == 200
         # assert data['parcels']['sender_id'] == 1
         self.assertIsInstance(data, dict)
 
     def test_status_is_required(self):
-        self.app.post('/api/v1/parcels', json=self.parcel)
+        self.app.post('/api/v1/parcels', json=self.test_parcel)
         response =self.app.put('/api/v1/parcels/1', json={"status": "", "sender": 1})
         data = json.loads(response.get_data(as_text=True))
         assert data["error"] == "status is required"
         assert response.status_code == 200
 
     def test_sender_is_required(self):
-        self.app.post('/api/v1/parcels', json=self.parcel)
+        self.app.post('/api/v1/parcels', json=self.test_parcel)
         response =self.app.put('/api/v1/parcels/1', json={"status": "cancelled", "sender": ""})
         data = json.loads(response.get_data(as_text=True))
         assert data["error"] == "sender is required"
         assert response.status_code == 200    
 
-    # def test_parcel_not_yours(self):
-    #     self.app.post('/api/v1/parcels', json=self.parcel)
-    #     response =self.app.get('/api/v1/parcels/sender/1')
-    #     data = json.loads(response.get_data(as_text=True))
-    #     assert data["message"] == "parcel not yours"
+    def test_weight_required(self):
+        required = { 
+            "country": "uganda",
+            "destination": "kampala",
+            "parcel_price": "500k",
+            "parcel_weight": "",
+            "pickup_location": "",
+            "recepient_name": "kampala",
+            "recepient_phone": "25706196611",
+            "sender_id": 1}
+        self.app.post('/api/v1/parcels', json=required)
+        response =self.app.get('/api/v1/parcels')
+        assert response.status_code == 200  
+
+
+    def test_price_required(self):
+        required = { 
+            "country": "uganda",
+            "destination": "kampala",
+            "parcel_price": "",
+            "parcel_weight": "5kg",
+            "pickup_location": "nairobi",
+            "recepient_name": "kampala",
+            "recepient_phone": "25706196611",
+            "sender_id": 1}
+        self.app.post('/api/v1/parcels', json=required)
+        response =self.app.get('/api/v1/parcels')
+        assert response.status_code == 200        
         
+    def test_recipient_name_is_required(self):
+        required = { 
+            "country": "uganda",
+            "destination": "kampala",
+            "parcel_price": "",
+            "parcel_weight": "5kg",
+            "pickup_location": "nairobi",
+            "recepient_name": "",
+            "recepient_phone": "25706196611",
+            "sender_id": 1}
+        self.app.post('/api/v1/parcels', json=required)
+        response =self.app.get('/api/v1/parcels')
+        assert response.status_code == 200        
+            
         
 
 
